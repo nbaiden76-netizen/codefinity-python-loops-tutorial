9d87728cb3b2f7c7be931fc8d5e970746422b26f
nbaiden76-netizen <>
2026-03-09 22:22:18 +0000

    Range iteration

---
+++ the_for_loop/range_iteration/main.py
@@ -0,0 +1,27 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List of countries you agreed to visit
+your_travel_list = []
+
+for i in range(0, len(countries), 2):
+    your_travel_list.append(countries[i])
+
+# Testing
+    print(your_travel_list)
+
+# List of travel destinations
+#travel_list = ['Monako', 'Luxemburg', 'Liverpool', 'Barcelona', 'Munchen']
+
+# Printing the first three destinations
+#for i in range(3):
+#    print(i, ':', travel_list[i])
+
+
+#travel_list = ['Monako', 'Luxemburg', 'Liverpool', 'Barcelona', 'Munchen']
+
+# Printing travel destinations in reverse
+# Start at last index, end at -1, step -1
+#for i in range(len(travel_list) - 1, -1, -1): 
+#    print(i, ':', travel_list[i])
+#
