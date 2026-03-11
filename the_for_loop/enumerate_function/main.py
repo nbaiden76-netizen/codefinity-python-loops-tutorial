8d7e05e2bc8f502a0a366da5b8d77c0fa5ade3b4
nbaiden76-netizen <>
2026-03-11 22:51:13 +0000

    Enumerate function

---
+++ the_for_loop/enumerate_function/main.py
@@ -0,0 +1,21 @@
+# Full list of countries you are considering for travel
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# Initialize an empty list for rankings
+travel_rankings = []
+
+# Use enumerate to label each destination
+
+for index, country in enumerate(countries,start=1):
+    travel_rankings.append(f"Destination {index}: {country}")
+
+
+# Testing
+print("Travel destination rankings:", travel_rankings)
+
+#travel_list = ['Monako', 'Luxemburg', 'Liverpool', 'Barcelona', 'Munchen']
+
+# Printing all cities with their indexes
+#for index, city in enumerate(travel_list):
+ #   print(str(index) + ' ' + city)
+
