0086e0e6611845b0b58dc821a369edc852d7f406
nbaiden76-netizen <>
2026-03-12 22:11:42 +0000

    While loops

---
+++ the_while_loop/while_loops/main.py
@@ -0,0 +1,28 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List to hold the selected countries
+selected = []
+
+i = 0 
+
+while i < len (countries) // 2:
+    selected.append(countries[i])
+    i += 1
+    
+
+# Testing
+print('Selected countries:', selected)
+
+
+
+#travel_list = ['Monako', 'Luxemburg', 'Liverpool', 'Barcelona', 'Munchen']
+
+# Initialize the index
+#i = 0
+
+# Print each destination using a while loop
+#while i < len(travel_list):
+ #   print(travel_list[i])
+ #   i += 1
+
